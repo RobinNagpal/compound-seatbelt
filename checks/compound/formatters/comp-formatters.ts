2fa035cb43ad5db88b25ae3f99781060d62b0528
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-01-26 22:33:16 +0500

    Added dynamic links in comp formatter

--- checks/compound/formatters/comp-formatters.ts
+++ checks/compound/formatters/comp-formatters.ts
@@ -12,16 +12,15 @@ export const compFormatters: { [functionName: string]: TransactionFormatter } = 
     const { contractName } = await getContractNameAndAbiFromFile(chain, decodedParams[0])
     const divisor = constants.WeiPerEther
     const amount = BigNumber.from(decodedParams[1]).div(divisor)
-    return `Grant **${amount} COMP** to **${contractName}**`
+    return `Approve **${amount}** [COMP](https://etherscan.io/address/0xc00e94cb662c3520282e6f5717214004a7f26888) to **[${contractName}](https://etherscan.io/address/${decodedParams[0]})**`
   },
   'transfer(address,uint256)': async (
     chain: CometChains,
     transaction: ExecuteTransactionInfo,
     decodedParams: string[]
   ) => {
-    const { contractName } = await getContractNameAndAbiFromFile(chain, decodedParams[0])
     const divisor = constants.WeiPerEther
     const amount = BigNumber.from(decodedParams[1]).div(divisor)
-    return `Transfer **${amount} COMP** to **${contractName}**`
+    return `Transfer **${amount}** [COMP](https://etherscan.io/address/0xc00e94cb662c3520282e6f5717214004a7f26888) to [Wallet](https://etherscan.io/address/${decodedParams[0]})`
   },
 }
