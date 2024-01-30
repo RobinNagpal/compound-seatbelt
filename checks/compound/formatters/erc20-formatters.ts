cefb2f9564a095fbd474dedf6012adfaafaea7f7
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-01-30 19:38:20 +0500

    Added approve function to ERC20 formatters

---
+++ checks/compound/formatters/erc20-formatters.ts
@@ -0,0 +1,46 @@
+import { Contract } from 'ethers'
+import { customProvider } from '../../../utils/clients/ethers'
+import { getContractNameAndAbiFromFile } from '../abi-utils'
+import { CometChains, ExecuteTransactionInfo, TransactionFormatter } from '../compound-types'
+import { defactor, getPlatform } from './helper'
+
+export const ERC20Formatters: { [functionName: string]: TransactionFormatter } = {
+  'transfer(address,uint256)': async (
+    chain: CometChains,
+    transaction: ExecuteTransactionInfo,
+    decodedParams: string[]
+  ) => {
+    console.log(`decodedParams ${decodedParams.join(',')}`)
+    const platform = await getPlatform(chain)
+
+    const { abi } = await getContractNameAndAbiFromFile(chain, transaction.target)
+    const currentInstance = new Contract(transaction.target, abi, customProvider(chain))
+    const symbol = await currentInstance.callStatic.symbol()
+    const decimals = await currentInstance.callStatic.decimals()
+    const compToken = defactor(BigInt(decodedParams[1]), parseFloat(`1e${decimals}`))
+
+    return `\n\nTransfer **${compToken.toFixed(2)} [${symbol}](https://${platform}/address/${
+      transaction.target
+    })** to ${decodedParams[0]}.`
+  },
+  'approve(address,uint256)': async (
+    chain: CometChains,
+    transaction: ExecuteTransactionInfo,
+    decodedParams: string[]
+  ) => {
+    console.log(`decodedParams ${decodedParams.join(',')}`)
+    const platform = await getPlatform(chain)
+
+    const { contractName } = await getContractNameAndAbiFromFile(chain, decodedParams[0])
+
+    const { abi: compAddressAbi } = await getContractNameAndAbiFromFile(chain, transaction.target)
+    const compInstance = new Contract(transaction.target, compAddressAbi, customProvider(chain))
+    const symbol = await compInstance.callStatic.symbol()
+
+    const compToken = defactor(BigInt(decodedParams[1]))
+
+    return `\n\nApprove **${compToken.toFixed(2)} [${symbol}](https://${platform}/address/${
+      transaction.target
+    })** tokens to [${contractName}](https://${platform}/address/${decodedParams[0]})`
+  },
+}
