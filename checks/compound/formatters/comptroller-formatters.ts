6ab300e7c9adfd02fc41abc07b0fb50ff33e40de
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-01-30 18:19:01 +0500

    Made new comptroller formatter and added function to it

---
+++ checks/compound/formatters/comptroller-formatters.ts
@@ -0,0 +1,31 @@
+import { Contract } from 'ethers'
+import { customProvider } from '../../../utils/clients/ethers'
+import { getContractNameAndAbiFromFile } from './../abi-utils'
+import { CometChains, ExecuteTransactionInfo, TransactionFormatter } from './../compound-types'
+import { defactor, getPlatform } from './helper'
+
+export const comptrollerFormatters: { [functionName: string]: TransactionFormatter } = {
+  '_grantComp(address,uint256)': async (
+    chain: CometChains,
+    transaction: ExecuteTransactionInfo,
+    decodedParams: string[]
+  ) => {
+    console.log(`decodedParams ${decodedParams.join(',')}`)
+    const platform = await getPlatform(chain)
+
+    const { contractName } = await getContractNameAndAbiFromFile(chain, decodedParams[0])
+    const { abi } = await getContractNameAndAbiFromFile(chain, transaction.target)
+    const currentInstance = new Contract(transaction.target, abi, customProvider(chain))
+
+    const compAddress = await currentInstance.callStatic.getCompAddress()
+    const { abi: compAddressAbi } = await getContractNameAndAbiFromFile(chain, compAddress)
+    const compInstance = new Contract(compAddress, compAddressAbi, customProvider(chain))
+    const symbol = await compInstance.callStatic.symbol()
+
+    const compToken = defactor(BigInt(decodedParams[1]))
+
+    return `\n\nGrant ${compToken.toFixed(2)} [${symbol}](https://${platform}/address/${
+      transaction.target
+    }) tokens to [${contractName}](https://${platform}/address/${decodedParams[0]})`
+  },
+}
