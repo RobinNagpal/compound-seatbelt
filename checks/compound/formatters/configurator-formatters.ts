f5ad454e17027f16b7dd3c62a11cfffe040106b3
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-01-31 20:23:23 +0500

    Added new functions to configurator formatters

--- checks/compound/formatters/configurator-formatters.ts
+++ checks/compound/formatters/configurator-formatters.ts
@@ -146,17 +146,102 @@ export const configuratorFormatters: { [functionName: string]: TransactionFormat
     const { symbol } = await getContractSymbolAndDecimalsFromFile(baseToken, baseTokenInstance, chain)
 
     const prevBorrowSpeed = defactor(await currentCometInstance.callStatic.baseTrackingBorrowSpeed())
-    // const previousRateInPercent = prevInterestRate * 100
     console.log(`Previous BaseTrackingBorrowSpeed: ${prevBorrowSpeed}`)
 
     const newBorrowSpeed = defactor(BigInt(decodedParams[1]))
-    // const currentRateInPercent = defactor(newInterestRate) * 100
     console.log(`New BaseTrackingBorrowSpeed: ${newBorrowSpeed}`)
 
     const changeInSpeed = calculateDifferenceOfDecimals(newBorrowSpeed, prevBorrowSpeed)
 
     return `\n\nSet BaseTrackingBorrowSpeed of [${symbol}](https://${platform}/address/${baseToken}) to ${newBorrowSpeed}. Previous value was ${prevBorrowSpeed} and now it is getting ${
       changeInSpeed > 0 ? 'increased' : 'decreased'
-    } by **${changeInSpeed}%**`
+    } by **${changeInSpeed}**`
+  },
+  'setBaseTrackingSupplySpeed(address,uint64)': async (
+    chain: CometChains,
+    transaction: ExecuteTransactionInfo,
+    decodedParams: string[]
+  ) => {
+    console.log(`decodedParams ${decodedParams.join(',')}`)
+    const platform = await getPlatform(chain)
+
+    const { abi } = await getContractNameAndAbiFromFile(chain, decodedParams[0])
+    const currentCometInstance = new Contract(decodedParams[0], abi, customProvider(chain))
+
+    const baseToken = await currentCometInstance.callStatic.baseToken()
+    const { abi: baseTokenAbi } = await getContractNameAndAbiFromFile(chain, baseToken)
+    const baseTokenInstance = new Contract(baseToken, baseTokenAbi, customProvider(chain))
+    const { symbol } = await getContractSymbolAndDecimalsFromFile(baseToken, baseTokenInstance, chain)
+
+    const prevSupplySpeed = defactor(await currentCometInstance.callStatic.baseTrackingSupplySpeed())
+    console.log(`Previous BaseTrackingBorrowSpeed: ${prevSupplySpeed}`)
+
+    const newSupplySpeed = defactor(BigInt(decodedParams[1]))
+    console.log(`New BaseTrackingBorrowSpeed: ${newSupplySpeed}`)
+
+    const changeInSpeed = calculateDifferenceOfDecimals(newSupplySpeed, prevSupplySpeed)
+
+    return `\n\nSet BaseTrackingSupplySpeed of [${symbol}](https://${platform}/address/${baseToken}) to ${newSupplySpeed}. Previous value was ${prevSupplySpeed} and now it is getting ${
+      changeInSpeed > 0 ? 'increased' : 'decreased'
+    } by **${changeInSpeed}**`
+  },
+  'setBorrowKink(address,uint64)': async (
+    chain: CometChains,
+    transaction: ExecuteTransactionInfo,
+    decodedParams: string[]
+  ) => {
+    console.log(`decodedParams ${decodedParams.join(',')}`)
+    const platform = await getPlatform(chain)
+
+    const { abi } = await getContractNameAndAbiFromFile(chain, decodedParams[0])
+
+    const currentCometInstance = new Contract(decodedParams[0], abi, customProvider(chain))
+    // console.log('current comet instance', currentCometInstance)
+    console.log('chain', chain)
+    const baseToken = await currentCometInstance.callStatic.baseToken()
+    console.log('baseToken', baseToken)
+    const { abi: baseTokenAbi } = await getContractNameAndAbiFromFile(chain, baseToken)
+    const baseTokenInstance = new Contract(baseToken, baseTokenAbi, customProvider(chain))
+    const { symbol } = await getContractSymbolAndDecimalsFromFile(baseToken, baseTokenInstance, chain)
+
+    const prevBorrowKink = defactor(await currentCometInstance.callStatic.borrowKink())
+    console.log(`Previous BorrowKink: ${prevBorrowKink}`)
+
+    const newBorrowKink = defactor(BigInt(decodedParams[1]))
+    console.log(`New BorrowKink: ${newBorrowKink}`)
+
+    const changeInKink = calculateDifferenceOfDecimals(newBorrowKink, prevBorrowKink)
+
+    return `\n\nSet BorrowKink of [${symbol}](https://${platform}/address/${baseToken}) to ${newBorrowKink}. Previous value was ${prevBorrowKink} and now it is getting ${
+      changeInKink > 0 ? 'increased' : 'decreased'
+    } by **${changeInKink}**`
+  },
+  'setSupplyKink(address,uint64)': async (
+    chain: CometChains,
+    transaction: ExecuteTransactionInfo,
+    decodedParams: string[]
+  ) => {
+    console.log(`decodedParams ${decodedParams.join(',')}`)
+    const platform = await getPlatform(chain)
+
+    const { abi } = await getContractNameAndAbiFromFile(chain, decodedParams[0])
+    const currentCometInstance = new Contract(decodedParams[0], abi, customProvider(chain))
+
+    const baseToken = await currentCometInstance.callStatic.baseToken()
+    const { abi: baseTokenAbi } = await getContractNameAndAbiFromFile(chain, baseToken)
+    const baseTokenInstance = new Contract(baseToken, baseTokenAbi, customProvider(chain))
+    const { symbol } = await getContractSymbolAndDecimalsFromFile(baseToken, baseTokenInstance, chain)
+
+    const prevSupplyKink = defactor(await currentCometInstance.callStatic.supplyKink())
+    console.log(`Previous SupplyKink: ${prevSupplyKink}`)
+
+    const newSupplyKink = defactor(BigInt(decodedParams[1]))
+    console.log(`New SupplyKink: ${newSupplyKink}`)
+
+    const changeInKink = calculateDifferenceOfDecimals(newSupplyKink, prevSupplyKink)
+
+    return `\n\nSet SupplyKink of [${symbol}](https://${platform}/address/${baseToken}) to ${newSupplyKink}. Previous value was ${prevSupplyKink} and now it is getting ${
+      changeInKink > 0 ? 'increased' : 'decreased'
+    } by **${changeInKink}**`
   },
 }
