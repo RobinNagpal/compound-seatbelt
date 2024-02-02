2af8fb55e6046ff2840f7fa03ace45035153f2ee
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-02-02 19:10:34 +0500

    Added new function setRewardConfig in configurator formatters

--- checks/compound/formatters/configurator-formatters.ts
+++ checks/compound/formatters/configurator-formatters.ts
@@ -348,4 +348,27 @@ export const configuratorFormatters: { [functionName: string]: TransactionFormat
       2
     )},\n\n**liquidationFactor:** ${liquidationFactor.toFixed(2)},\n\n**supplyCap:** ${supplyCap.toFixed(2)}\n\n}`
   },
+  'setRewardConfig(address,address)': async (
+    chain: CometChains,
+    transaction: ExecuteTransactionInfo,
+    decodedParams: string[]
+  ) => {
+    const platform = await getPlatform(chain)
+
+    console.log(`decodedParams ${decodedParams.join(',')}`)
+
+    const { abi } = await getContractNameAndAbiFromFile(chain, decodedParams[0])
+    const currentInstance = new Contract(decodedParams[0], abi, customProvider(chain))
+
+    const baseToken = await currentInstance.callStatic.baseToken()
+    const { abi: baseTokenAbi } = await getContractNameAndAbiFromFile(chain, baseToken)
+    const baseTokenInstance = new Contract(baseToken, baseTokenAbi, customProvider(chain))
+    const { symbol: tokenSymbol } = await getContractSymbolAndDecimalsFromFile(baseToken, baseTokenInstance, chain)
+
+    const { abi: compAbi } = await getContractNameAndAbiFromFile(chain, decodedParams[1])
+    const compInstance = new Contract(decodedParams[1], compAbi, customProvider(chain))
+    const { symbol } = await getContractSymbolAndDecimalsFromFile(decodedParams[1], compInstance, chain)
+
+    return `\n\nSet reward token for market **[${tokenSymbol}](https://${platform}/address/${baseToken})** as **[${symbol}](https://${platform}/address/${decodedParams[1]})**`
+  },
 }
