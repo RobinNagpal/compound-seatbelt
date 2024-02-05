dd32bbf42aac08a36ac68adb6bf8aa1b18f25018
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-02-05 19:14:42 +0500

    Added functions depositForBurn and setText in erc20 formatters

--- checks/compound/formatters/erc20-formatters.ts
+++ checks/compound/formatters/erc20-formatters.ts
@@ -3,9 +3,13 @@ import { customProvider } from '../../../utils/clients/ethers'
 import { getContractNameAndAbiFromFile } from '../abi-utils'
 import { CometChains, ExecuteTransactionInfo, TransactionFormatter } from '../compound-types'
 import {
+  defactor,
+  getContractSymbolAndDecimalsFromFile,
+  getFormatCompTokens,
   getFormattedTokenNameWithLink,
   getFormattedTokenWithLink,
   getPercentageForTokenFactor,
+  getPlatform,
   getRecipientNameWithLink,
 } from './helper'
 
@@ -46,5 +50,39 @@ export const ERC20Formatters: { [functionName: string]: TransactionFormatter } =
     }
 
     return `\n\nSet reserve factor for ${tokenNameWithLink} to ${newReserveFactor}%`
+  },
+  'depositForBurn(uint256,uint32,bytes32,address)': async (
+    chain: CometChains,
+    transaction: ExecuteTransactionInfo,
+    decodedParams: string[]
+  ) => {
+    const platform = await getPlatform(chain)
+
+    const { contractName } = await getContractNameAndAbiFromFile(chain, transaction.target)
+
+    const burnContractAddress = decodedParams[3]
+    const { abi } = await getContractNameAndAbiFromFile(chain, burnContractAddress)
+    const tokenInstance = new Contract(burnContractAddress, abi, customProvider(chain))
+    const { symbol: tokenSymbol, decimals } = await getContractSymbolAndDecimalsFromFile(
+      burnContractAddress,
+      tokenInstance,
+      chain
+    )
+
+    const amount = defactor(BigInt(decodedParams[0]), parseFloat(`1e${decimals}`))
+
+    return `\n\nSet DepositforBurn of ${contractName} for the Burn contract [${tokenSymbol}](https://${platform}/address/${burnContractAddress}) with amount ${amount.toFixed(
+      2
+    )}, destination domain ${decodedParams[1]} and the Mint recipient ${decodedParams[2]}`
+  },
+  'setText(bytes32,string,string)': async (
+    chain: CometChains,
+    transaction: ExecuteTransactionInfo,
+    decodedParams: string[]
+  ) => {
+    const { abi } = await getContractNameAndAbiFromFile(chain, transaction.target)
+    const instance = new Contract(transaction.target, abi, customProvider(chain))
+    const name = await instance.callStatic.name(decodedParams[0])
+    return `\n\nSet ENS text with key: ${decodedParams[1]} and value:\n\n ${decodedParams[2]}`
   },
 }
