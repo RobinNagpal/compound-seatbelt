5ece61a06a10f80dcb45ed07dcfd38fe3b998920
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-02-09 21:25:50 +0500

    Added new functions setVotingDelay & setVotingPeriod in governor bravo formatters

--- checks/compound/formatters/governor-bravo-formatters.ts
+++ checks/compound/formatters/governor-bravo-formatters.ts
@@ -50,4 +50,46 @@ export const governorBravoFormatters: { [functionName: string]: TransactionForma
 
     return `\n\nSet the Whitelist Guardian of [${name}](https://${platform}/address/${governanceAddress}) to [${guardianContractName}](https://${platform}/address/${decodedParams[0]}).`
   },
+  '_setVotingDelay(uint256)': async (
+    chain: CometChains,
+    transaction: ExecuteTransactionInfo,
+    decodedParams: string[]
+  ) => {
+    const platform = await getPlatform(chain)
+
+    const governanceAddress = transaction.target
+    const { abi } = await getContractNameAndAbiFromFile(chain, governanceAddress)
+    const governanceInstance = new Contract(governanceAddress, abi, customProvider(chain))
+
+    const name = await governanceInstance.callStatic.name()
+    const prevVotingDelay = await governanceInstance.callStatic.votingDelay()
+    const newVotingDelay = parseFloat(decodedParams[0])
+
+    const changeInVotingDelay = await calculateDifferenceOfDecimals(newVotingDelay, prevVotingDelay)
+
+    return `\n\nNumber of Ethereum blocks to wait before voting on a proposal may begin (Voting Delay) for [${name}](https://${platform}/address/${governanceAddress}) is set to ${newVotingDelay} blocks. Previous value was ${prevVotingDelay} blocks and ${getChangeText(
+      changeInVotingDelay
+    )}.`
+  },
+  '_setVotingPeriod(uint256)': async (
+    chain: CometChains,
+    transaction: ExecuteTransactionInfo,
+    decodedParams: string[]
+  ) => {
+    const platform = await getPlatform(chain)
+
+    const governanceAddress = transaction.target
+    const { abi } = await getContractNameAndAbiFromFile(chain, governanceAddress)
+    const governanceInstance = new Contract(governanceAddress, abi, customProvider(chain))
+
+    const name = await governanceInstance.callStatic.name()
+    const prevVotingPeriod = await governanceInstance.callStatic.votingPeriod()
+    const newVotingPeriod = parseFloat(decodedParams[0])
+
+    const changeInVotingPeriod = await calculateDifferenceOfDecimals(newVotingPeriod, prevVotingPeriod)
+
+    return `\n\nThe duration of voting on a proposal in terms of Ethereum blocks (Voting Period) of [${name}](https://${platform}/address/${governanceAddress}) is set to ${newVotingPeriod} blocks. Previous value was ${prevVotingPeriod} blocks and ${getChangeText(
+      changeInVotingPeriod
+    )}.`
+  },
 }
