967632db0ec712ed5181351eb963231600f9b679
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-02-12 19:12:54 +0500

    Moved function to bridge formatters

--- checks/compound/formatters/bridge-formatters.ts
+++ checks/compound/formatters/bridge-formatters.ts
@@ -1,5 +1,14 @@
+import { customProvider } from '../../../utils/clients/ethers'
+import { getContractNameAndAbiFromFile } from '../abi-utils'
 import { CometChains, ExecuteTransactionInfo, TransactionFormatter } from './../compound-types'
-import { getFormattedTokenWithLink, getRecipientNameWithLink } from './helper'
+import {
+  defactor,
+  getContractSymbolAndDecimalsFromFile,
+  getFormattedTokenWithLink,
+  getPlatform,
+  getRecipientNameWithLink,
+} from './helper'
+import { Contract } from 'ethers'
 
 export const bridgeFormatters: { [functionName: string]: TransactionFormatter } = {
   'depositFor(address,address,bytes)': async (
@@ -16,5 +25,27 @@ export const bridgeFormatters: { [functionName: string]: TransactionFormatter } 
     }
 
     throw new Error('Unknown bridge contract')
+  },
+  'outboundTransferCustomRefund(address,address,address,uint256,uint256,uint256,bytes)': async (
+    chain: CometChains,
+    transaction: ExecuteTransactionInfo,
+    decodedParams: string[]
+  ) => {
+    const platform = await getPlatform(chain)
+
+    const tokenAddress = decodedParams[0]
+    const { abi: tokenAbi } = await getContractNameAndAbiFromFile(chain, tokenAddress)
+    const tokenInstance = new Contract(tokenAddress, tokenAbi, customProvider(chain))
+    const { symbol: tokenSymbol, decimals: tokenDecimals } = await getContractSymbolAndDecimalsFromFile(
+      tokenAddress,
+      tokenInstance,
+      chain
+    )
+    const amount = defactor(BigInt(decodedParams[3]), parseFloat(`1e${tokenDecimals}`))
+    const recipientWithLink = getRecipientNameWithLink(CometChains.arbitrum, decodedParams[2])
+
+    return `\n\nBridge ${amount.toFixed(
+      2
+    )} [${tokenSymbol}](https://${platform}/address/${tokenAddress}) tokens over Arbitrum to ${recipientWithLink}.`
   },
 }
