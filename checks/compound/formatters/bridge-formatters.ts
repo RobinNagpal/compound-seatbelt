2afe1cf7d93983b77ae646ccf4432a00cf367822
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-02-21 17:18:22 +0500

    Done with rounding function changes of bridge formatters

--- checks/compound/formatters/bridge-formatters.ts
+++ checks/compound/formatters/bridge-formatters.ts
@@ -11,6 +11,7 @@ import {
   getRecipientNameWithLink,
 } from './helper'
 import { Contract } from 'ethers'
+import { defactorFn } from './../../../utils/roundingUtils'
 
 export const bridgeFormatters: { [functionName: string]: TransactionFormatter } = {
   'depositFor(address,address,bytes)': async (chain: CometChains, transaction: ExecuteTransactionInfo, decodedParams: string[]) => {
@@ -35,10 +36,10 @@ export const bridgeFormatters: { [functionName: string]: TransactionFormatter } 
     const { abi: tokenAbi } = await getContractNameAndAbiFromFile(chain, tokenAddress)
     const tokenInstance = new Contract(tokenAddress, tokenAbi, customProvider(chain))
     const { symbol: tokenSymbol, decimals: tokenDecimals } = await getContractSymbolAndDecimalsFromFile(tokenAddress, tokenInstance, chain)
-    const amount = defactor(BigInt(decodedParams[3]), parseFloat(`1e${tokenDecimals}`))
+    const amount = defactorFn(decodedParams[3], `${tokenDecimals}`)
     const recipientWithLink = getRecipientNameWithLink(CometChains.arbitrum, decodedParams[2])
 
-    return `Bridge ${addCommas(amount.toFixed(2))} [${tokenSymbol}](https://${platform}/address/${tokenAddress}) tokens over Arbitrum to ${recipientWithLink}.`
+    return `Bridge ${amount} [${tokenSymbol}](https://${platform}/address/${tokenAddress}) tokens over Arbitrum to ${recipientWithLink}.`
   },
   'createStream(address,uint256,address,uint256,uint256)': async (chain: CometChains, transaction: ExecuteTransactionInfo, decodedParams: string[]) => {
     const platform = getPlatform(chain)
@@ -51,13 +52,11 @@ export const bridgeFormatters: { [functionName: string]: TransactionFormatter } 
     const tokenInstance = new Contract(tokenAddress, tokenAbi, customProvider(chain))
     const { symbol: tokenSymbol, decimals: tokenDecimals } = await getContractSymbolAndDecimalsFromFile(tokenAddress, tokenInstance, chain)
 
-    const amount = defactor(BigInt(decodedParams[1]), parseFloat(`1e${tokenDecimals}`))
+    const amount = defactorFn(decodedParams[1], `${tokenDecimals}`)
 
     const recipientWithLink = getRecipientNameWithLink(chain, recipientAddress)
 
-    return `Create a stream on [${senderName}](https://${platform}/address/${senderAddress}) to transfer **${addCommas(
-      amount.toFixed(2)
-    )}** [${tokenSymbol}](https://${platform}/address/${tokenAddress}) to ${recipientWithLink}. The stream will start at ${formatTimestamp(
+    return `Create a stream on [${senderName}](https://${platform}/address/${senderAddress}) to transfer **${amount}** [${tokenSymbol}](https://${platform}/address/${tokenAddress}) to ${recipientWithLink}. The stream will start at ${formatTimestamp(
       decodedParams[3]
     )} and end at ${formatTimestamp(decodedParams[4])}.`
   },
