81d55f15eef9ba0c9540edd2910b79a48236213d
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-03-28 22:11:20 +0500

    sablier formatter

--- checks/compound/formatters/sablier-formatters.ts
+++ checks/compound/formatters/sablier-formatters.ts
@@ -1,17 +1,22 @@
 import { getContractNameAndAbiFromFile } from '../abi-utils'
 import { CometChains, ExecuteTransactionInfo, TransactionFormatter } from '../compound-types'
-import { addCommas, formatTimestamp, getContractSymbolAndDecimalsFromFile, getPlatform, getRecipientNameWithLink } from './helper'
+import {
+  addCommas,
+  addressFormatter,
+  formatTimestamp,
+  getContractNameWithLink,
+  getContractSymbolAndDecimalsFromFile,
+  getPlatform,
+  getRecipientNameWithLink,
+} from './helper'
 import { customProvider } from './../../../utils/clients/ethers'
 import { defactorFn } from './../../../utils/roundingUtils'
 import { Contract } from 'ethers'
 
 export const sablierFormatters: { [functionName: string]: TransactionFormatter } = {
   'createStream(address,uint256,address,uint256,uint256)': async (chain: CometChains, transaction: ExecuteTransactionInfo, decodedParams: string[]) => {
-    const platform = getPlatform(chain)
-    const senderAddress = transaction.target
     const recipientAddress = decodedParams[0]
-    const { contractName: senderName } = await getContractNameAndAbiFromFile(chain, transaction.target)
-
+    const senderContractNameWithLink = await getContractNameWithLink(transaction.target, chain)
     const tokenAddress = decodedParams[2]
     const { abi: tokenAbi } = await getContractNameAndAbiFromFile(chain, tokenAddress)
     const tokenInstance = new Contract(tokenAddress, tokenAbi, customProvider(chain))
@@ -21,10 +26,10 @@ export const sablierFormatters: { [functionName: string]: TransactionFormatter }
 
     const recipientWithLink = await getRecipientNameWithLink(chain, recipientAddress)
 
-    return `🛑 Create a stream on **[${senderName}](https://${platform}/address/${senderAddress})** to transfer **${addCommas(
-      amount
-    )} [${tokenSymbol}](https://${platform}/address/${tokenAddress})** to ${recipientWithLink}. The stream will start at ${formatTimestamp(
-      decodedParams[3]
-    )} and end at ${formatTimestamp(decodedParams[4])}.`
+    const tokensWithLink = `**${addCommas(amount)} ${addressFormatter(tokenAddress, chain, tokenSymbol)}**`
+    const functionDesc = `Create a stream on ${senderContractNameWithLink} to transfer ${tokensWithLink} to ${recipientWithLink}`
+    const streamingInfo = `The stream will start at ${formatTimestamp(decodedParams[3])} and end at ${formatTimestamp(decodedParams[4])}.`
+
+    return `🛑 ${functionDesc}. ${streamingInfo}`
   },
 }
