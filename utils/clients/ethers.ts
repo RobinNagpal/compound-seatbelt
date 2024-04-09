7ec5bd258035c2659ca05d2d7b4194883f7f99dd
robin <robinnagpal.tiet@gmail.com>
2024-04-09 12:41:07 -0400

    Log URL

--- utils/clients/ethers.ts
+++ utils/clients/ethers.ts
@@ -12,7 +12,7 @@ import {
 export const provider = new providers.JsonRpcProvider(RPC_URL_MAINNET)
 
 export function customProvider(chain: CometChains) {
-  switch (chain) {
+  switch (chain.toString()) {
     case CometChains.mainnet:
       return new providers.JsonRpcProvider(RPC_URL_MAINNET)
     case CometChains.polygon:
@@ -24,6 +24,7 @@ export function customProvider(chain: CometChains) {
     case CometChains.scroll:
       return new providers.JsonRpcProvider(RPC_URL_SCROLL)
     case CometChains.optimism:
+      console.log('RPC_URL_OPTIMISM', RPC_URL_OPTIMISM)
       return new providers.JsonRpcProvider(RPC_URL_OPTIMISM)
     default:
       throw new Error('Unknown chain: ' + chain)
