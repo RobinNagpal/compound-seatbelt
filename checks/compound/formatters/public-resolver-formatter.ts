675ceda5a9f9d084aa5ce782556a6d51f40499f3
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-04-15 20:46:57 +0500

    added difference checking for ENS resolver

--- checks/compound/formatters/public-resolver-formatter.ts
+++ checks/compound/formatters/public-resolver-formatter.ts
@@ -1,4 +1,8 @@
+import { getContractNameAndAbiFromFile } from '../abi-utils'
 import { CometChains, ExecuteTransactionInfo, TransactionFormatter } from '../compound-types'
+import { Contract } from 'ethers'
+import { customProvider } from './../../../utils/clients/ethers'
+import diff from 'deep-diff'
 
 export const publicResolverFormatter: { [functionName: string]: TransactionFormatter } = {
   'setText(bytes32,string,string)': async (chain: CometChains, transaction: ExecuteTransactionInfo, decodedParams: string[]) => {
@@ -7,9 +11,52 @@ export const publicResolverFormatter: { [functionName: string]: TransactionForma
         ? 'v3-additional-grants.compound-community-licenses.eth'
         : 'Unknown ENS Name'
 
-    const jsonValue = JSON.parse(decodedParams[2])
-    const formattedJsonValue = JSON.stringify(jsonValue, null, 2)
+    const { abi: targetAbi } = await getContractNameAndAbiFromFile(chain, transaction.target)
+    const targetInstance = new Contract(transaction.target, targetAbi, customProvider(chain))
 
-    return `Set ENS text for ${ENSSubdomain} with key: ${decodedParams[1]} and value:\n\`\`\`json\n${formattedJsonValue}\n\`\`\``
+    const prevtext = await targetInstance.callStatic.text(decodedParams[0], decodedParams[1])
+    const changes = summarizeChanges(prevtext, decodedParams[2])
+
+    const formattedJsonValue = JSON.stringify(changes.message ?? changes, null, 2)
+    return `Set ENS text for ${ENSSubdomain} with key: ${decodedParams[1]} and value modification:\n\`\`\`json\n${formattedJsonValue}\n\`\`\``
   },
+}
+
+interface DiffResult {
+  Added?: string[]
+  Updated?: string[]
+  Deleted?: string[]
+  message?: string
+}
+
+function summarizeChanges(oldJsonStr: string, newJsonStr: string): DiffResult {
+  const oldJson = JSON.parse(oldJsonStr)
+  const newJson = JSON.parse(newJsonStr)
+
+  const result: DiffResult = {}
+  const difference = diff(oldJson, newJson)
+
+  if (!difference) {
+    result.message = 'No Change.'
+    return result
+  }
+
+  difference.forEach((change) => {
+    const path = change.path ? change.path.join('.') : ''
+    switch (change.kind) {
+      case 'N':
+        if (!result.Added) result.Added = []
+        result.Added.push(`'${path}': ${JSON.stringify(change.rhs)}`)
+        break
+      case 'E':
+        if (!result.Updated) result.Updated = []
+        result.Updated.push(`'${path}': from ${JSON.stringify(change.lhs)} to ${JSON.stringify(change.rhs)}`)
+        break
+      case 'D':
+        if (!result.Deleted) result.Deleted = []
+        result.Deleted.push(`'${path}': ${JSON.stringify(change.lhs)}`)
+        break
+    }
+  })
+  return result
 }
