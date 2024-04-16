00008e79b73ed8836aff350aecda0c5aa8481f97
robin <robinnagpal.tiet@gmail.com>
2024-04-16 07:39:52 -0400

    Update how json diff is calculated

--- checks/compound/formatters/public-resolver-formatter.ts
+++ checks/compound/formatters/public-resolver-formatter.ts
@@ -2,7 +2,7 @@ import { getContractNameAndAbiFromFile } from '../abi-utils'
 import { CometChains, ExecuteTransactionInfo, TransactionFormatter } from '../compound-types'
 import { Contract } from 'ethers'
 import { customProvider } from './../../../utils/clients/ethers'
-import diff, { DiffArray, DiffNew, DiffDeleted, DiffEdit } from 'deep-diff'
+import { diffString } from 'json-diff'
 
 export const publicResolverFormatter: { [functionName: string]: TransactionFormatter } = {
   'setText(bytes32,string,string)': async (chain: CometChains, transaction: ExecuteTransactionInfo, decodedParams: string[]) => {
@@ -15,88 +15,15 @@ export const publicResolverFormatter: { [functionName: string]: TransactionForma
     const targetInstance = new Contract(transaction.target, targetAbi, customProvider(chain))
 
     const prevtext = await targetInstance.callStatic.text(decodedParams[0], decodedParams[1])
-    const changes = summarizeChanges(prevtext, decodedParams[2])
+    const oldJsonStr = prevtext
+    const newJsonStr = decodedParams[2]
 
-    const formattedJsonValue = JSON.stringify(changes.message ?? changes, null, 2)
-    return `Set ENS text for ${ENSSubdomain} with key: ${decodedParams[1]} and value modification:\n\`\`\`json\n${formattedJsonValue}\n\`\`\``
+    const oldJson = JSON.parse(oldJsonStr)
+    const newJson = JSON.parse(newJsonStr)
+    const changes = diffString(oldJson, newJson, { color: false, verbose: true })
+
+    const jsonDiffChangeStr = `\n \`\`\`json\n${changes}\n\`\`\``
+
+    return `Set ENS text for ${ENSSubdomain} with key: ${decodedParams[1]} and updates -  \n\n${jsonDiffChangeStr}`
   },
-}
-
-interface DiffResult {
-  Added?: { [key: string]: any }
-  Updated?: { [key: string]: any }
-  Deleted?: { [key: string]: any }
-  ArrayChanges?: { [key: string]: ArrayChange[] }
-  message?: string
-}
-
-interface ArrayChange {
-  index: number
-  changeType: 'insert' | 'delete' | 'update'
-  value?: any
-  previousValue?: any
-}
-
-function summarizeChanges(oldJsonStr: string, newJsonStr: string): DiffResult {
-  const oldJson = JSON.parse(oldJsonStr)
-  const newJson = JSON.parse(newJsonStr)
-
-  const result: DiffResult = {}
-  const difference = diff(oldJson, newJson)
-  if (!difference) {
-    result.message = 'No Change.'
-    return result
-  }
-
-  difference.forEach((change) => {
-    const path = change.path ? change.path.join('.') : ''
-    switch (change.kind) {
-      case 'N':
-        if (!result.Added) result.Added = {}
-        result.Added[path] = change.rhs
-        break
-      case 'E':
-        if (!result.Updated) result.Updated = {}
-        result.Updated[path] = { from: change.lhs, to: change.rhs }
-        break
-      case 'D':
-        if (!result.Deleted) result.Deleted = {}
-        result.Deleted[path] = change.lhs
-        break
-      case 'A':
-        if (!result.ArrayChanges) result.ArrayChanges = {}
-        if (!result.ArrayChanges[path]) result.ArrayChanges[path] = []
-        handleArrayChange(result.ArrayChanges[path], change)
-        break
-    }
-  })
-  return result
-}
-
-function handleArrayChange(arrayChanges: ArrayChange[], change: DiffArray<any, any>) {
-  const item = change.item
-
-  if (item.kind === 'N') {
-    const newItem = item as DiffNew<any>
-    arrayChanges.push({
-      index: change.index,
-      changeType: 'insert',
-      value: newItem.rhs,
-    })
-  } else if (item.kind === 'D') {
-    const deletedItem = item as DiffDeleted<any>
-    arrayChanges.push({
-      index: change.index,
-      changeType: 'delete',
-      previousValue: deletedItem.lhs,
-    })
-  } else if (item.kind === 'E') {
-    const editItem = item as DiffEdit<any, any>
-    arrayChanges.push({
-      index: change.index,
-      changeType: 'update',
-      value: editItem.rhs,
-      previousValue: editItem.lhs,
-    })
-  }
 }
